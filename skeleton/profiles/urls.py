257c213964fcac9f6f82e0df621c92ea4db39aa9
dbshow <dbrownbeta@gmail.com>
2013-08-15 15:35:49 -0400

    style fixes

--- skeleton/profiles/urls.py
+++ skeleton/profiles/urls.py
@@ -4,4 +4,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('profiles.views',
     url(r'^(?P<profile_id>\d+)/(?P<profile_slug>\w+)', 'profile_view', name='profile'),
 
-        )
+)
