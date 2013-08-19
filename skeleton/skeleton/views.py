ef11cff9a7d4b740644309887cf93e880d9c30f2
dbrownbeta <dbrownbeta@gmail.com>
2013-08-19 00:11:29 -0400

    getting views ready for tomorrow

--- skeleton/skeleton/views.py
+++ skeleton/skeleton/views.py
@@ -28,7 +28,7 @@ def create_profile(request, user_id):
     user = get_object_or_404(User, id=user_id)
     profile = user.profile
     if request.user != user:
-        return redirect('index')
+        return redirect('index') # decorator
     if profile.clean_fields():
         return redirect('profile', user_id, user.slug)
     if request.method == 'POST':
@@ -46,33 +46,9 @@ def profile(request, user_id, user_slug):
     return render_to_response('profile.html', RequestContext(request,{}))
 
 
-"""def profile(request, user_id, user_slug):
+def edit_profile(request, user_id, user_slug):
     user = get_object_or_404(User, id=user_id)
     profile = get_object_or_404(UserProfileModel)
-    if request.user != profile_user:
+    if request.user != profile_user: #decorator
         return redirect('index')
-    if request.method == 'POST':
-        edited_user_form = UserCreationForm(request.POST, instance=user)
-        edited_profile_from = UserProfileForm(request.POST, instance=profile)
-        if edited_user_form.is_valid() and edited_profile_from.is_valid():
-            new_user = new_user_form.save(commit=False)
-            new_profile = new_profile_form.save(commit=False)
-            new_user.first_name = new_profile.first_name
-            new_user.last_name = new_profile.last_name
-            new_user.email = new_profile.email
-            new_user.save()
-            new_profile.user = new_user
-            new_profile.save()
-            authenticated_user = authenticate(
-                username=request.POST['username'],
-                password=request.POST['password1']
-                )
-            login(request, authenticated_user)
-            return redirect('index')
-    else:
-        edited_user_form = UserCreationForm(instance=user)
-        edited_profile_from = UserProfileForm(instance=profile)
-    return render_to_response('new_user.html', RequestContext(request,{
-        'new_user_form': new_user_form,
-        'new_profile_form': new_profile_form,       
-    }))"""
+    pass
