89092dcf721dda173039cb6fc9a7557525fbe983
dbrownbeta <dbrownbeta@gmail.com>
2013-08-27 16:09:53 -0400

    working on bibliopage, bug in view

--- skeleton/projects/urls.py
+++ skeleton/projects/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('projects.views',
     url(r'^$', 'index', name='index'),
+    url(r'^bibliography', 'bibliography', name='bibliography'),
     url(r'^research/(?P<research_id>\d+)/(?P<research_slug>.+)', 'research_line', name='research_line'),
 
     )
