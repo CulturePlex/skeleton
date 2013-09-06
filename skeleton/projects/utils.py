f2fe25a50ac81a1ff5892d24a615256bbb2db481
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-09-06 00:53:20 -0400

    fixed some typos, credited code

--- skeleton/projects/utils.py
+++ skeleton/projects/utils.py
@@ -5,6 +5,9 @@ from itertools import chain
 
 from django.db.models import Q, CharField, TextField
 
+#####################################################################
+# From Julian Phalip 
+# http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap
 def normalize_query(query_string,
                     findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
                     normspace=re.compile(r'\s{2,}').sub):
@@ -12,11 +15,11 @@ def normalize_query(query_string,
 
 def get_query(query_string, search_fields):
     q_set = None
-    terms = normalize_query(query_string)
-    for term in terms:
+    search_terms = normalize_query(query_string)
+    for term in search_terms:
         or_query = None
         for field_name in search_fields:
-            q = Q((u'{0}__icontains'.format(field)=term)
+            q = Q((u'{0}__icontains'.format(field_name)=term)
             if or_query is None:
                 or_query = q
             else:
@@ -27,10 +30,13 @@ def get_query(query_string, search_fields):
             q_set = q_set & or_query    
     return q_set
 
+#####################################################################
+
 def model_search(model, query_string):
-    search_fields = [field for field in model._meta.get_all_field_names()
-                     if field isinstance(field, CharField)
-                     or if field isinstance(field, TextField)]
+    search_fields = [
+        field for field in model._meta.get_all_field_names() 
+        if isinstance(field, CharField) or if isinstance(field, TextField)
+        ]
     q_set = get_query(query_string, search_fields)
     return model.objects.filter(q_set)
 
