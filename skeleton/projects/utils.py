312b068561fc8dad01ad1f499f48aa7d66c91c49
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-09-06 00:28:48 -0400

    fixing utils file, moved to project

--- skeleton/projects/utils.py
+++ skeleton/projects/utils.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*- 
 import re
 
-from django.db.models import Q
+from itertools import chain
+
+from django.db.models import Q, CharField, TextField
 
 def normalize_query(query_string,
                     findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
@@ -9,12 +11,12 @@ def normalize_query(query_string,
     return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
 
 def get_query(query_string, search_fields):
-    query = None
+    q_set = None
     terms = normalize_query(query_string)
     for term in terms:
         or_query = None
         for field_name in search_fields:
-            q = Q(**{"%s__icontains" % field_name: term})
+            q = Q((u'{0}__icontains'.format(field)=term)
             if or_query is None:
                 or_query = q
             else:
@@ -22,17 +24,21 @@ def get_query(query_string, search_fields):
         if query is None:
             query = or_query
         else:
-            query = query & or_query
-    return query
+            q_set = q_set & or_query    
+    return q_set
 
 def model_search(model, query_string):
-	model_string = model.str()
-	search_fields = [u'model.string__{0}'.format(field) for field in model._meta.get_all_field_names()]
-	query_results = get_query(query_string, search_fields)
-	return model.filter(query_results)
-	
+    search_fields = [field for field in model._meta.get_all_field_names()
+                     if field isinstance(field, CharField)
+                     or if field isinstance(field, TextField)]
+    q_set = get_query(query_string, search_fields)
+    return model.objects.filter(q_set)
+
 def multi_model_search(model_list, query_string):
-	results = []
-	for model in model_list:
-		results += model_search(model, query_string)
-	return results
+    results = []
+    for model in model_list:
+        results.append(model_search(model, query_string))
+    results = chain.from_iterable(results)
+    return results
+
+    
