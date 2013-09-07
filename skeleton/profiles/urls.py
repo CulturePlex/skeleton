080c871831cd40e524acc4fde5ab2add2d5699b9
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-09-07 15:40:59 -0400

    removing profiles app

--- skeleton/profiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('profiles.views',
-    url(r'^(?P<profile_id>\d+)/(?P<profile_slug>\w+)', 'profile_view', name='profile'),
-
-)
